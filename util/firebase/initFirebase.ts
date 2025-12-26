ad6928faac9a29949496188d953ef8434eb2ef13
Hellicat <jhccop@naver.com>
2025-12-26 20:14:37 +0900

    [Chore] Edit Code

--- util/firebase/initFirebase.ts
+++ util/firebase/initFirebase.ts
@@ -1,25 +1,3 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getMessaging, getToken} from "firebase/messaging";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAtqbsz5KHfSI5FpLfZa98pGqFOCBlg9aE",
-//   authDomain: "project1-bb5e2.firebaseapp.com",
-//   projectId: "project1-bb5e2",
-//   storageBucket: "project1-bb5e2.firebasestorage.app",
-//   messagingSenderId: "729098212038",
-//   appId: "1:729098212038:web:e24b600bb98334d111bdc7",
-//   measurementId: "G-MG8SMMEV3Y",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const messaging = getMessaging(app);
-// export const fcmToken = getToken(messaging, {
-//   vapidKey:
-//     "BDvAfhYQkGZBR6_A_NLM2jMamkTgaHVlIlU3NjkN_6d1JSKexIcf5n9TKfSfOnVTfW6PqDXn9h4_OkCs__JSdiE",
-// });
